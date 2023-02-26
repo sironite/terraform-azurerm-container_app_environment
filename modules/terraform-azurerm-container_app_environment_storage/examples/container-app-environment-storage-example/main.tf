b41cbb90d2316c245921cfea814b4bf1fb8e6259
WDamman <58424695+TheIronRock95@users.noreply.github.com>
2023-02-26 14:31:04 +0100

    docs: update storage example

--- modules/terraform-azurerm-container_app_environment_storage/examples/container-app-environment-storage-example/main.tf
+++ modules/terraform-azurerm-container_app_environment_storage/examples/container-app-environment-storage-example/main.tf
@@ -1,14 +1,28 @@
-module "container_app_environment_terraform-azurerm-container_app_environment_storage" {
+module "container_app_environment" {
+  source  = "sironite/container_app_environment/azurerm"
+  version = "x.x.x"
+
+  container_app_environment_name  = var.container_app_environment_name
+  azure_location                  = var.azure_location
+  resource_group_name             = var.resource_group_name
+
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  infrastructure_subnet_id       = var.infrastructure_subnet_id
+  internal_load_balancer_enabled = var.internal_load_balancer_enabled
+
+  tags = var.container_app_environment_tags
+}
+
+module "azurerm_container_app_environment_storage" {
   source  = "sironite/container_app_environment/azurerm//modules/terraform-azurerm-container_app_environment_storage"
   version = "x.x.x"
 
-  name                         = var.container_app_environment_storage_name
-  container_app_environment_id = var.container_app_environment_id
+  container_app_environment_storage_name = var.container_app_environment_storage_name
+  container_app_environment_id           = module.container_app_environment.container_app_environment_id
 
-  account_name                 = var.storage_account_name
-  share_name                   = var.storage_share_name
-  access_key                   = var.storage_account_access_key
-  access_mode                  = var.storage_access_mode
+  storage_account_name       = var.storage_account_name
+  storage_share_name         = var.storage_share_name
+  storage_account_access_key = var.storage_account_access_key
+  storage_access_mode        = var.storage_access_mode
 }
-
-
--- modules/terraform-azurerm-container_app_environment_storage/examples/container-app-environment-storage-example/variables.tf
+++ modules/terraform-azurerm-container_app_environment_storage/examples/container-app-environment-storage-example/variables.tf
@@ -1,3 +1,23 @@
+variable "azure_location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  type        = string
+  sensitive   = false
+}
+variable "container_app_environment_name" {
+  description = "The name of the Container Apps Managed Environment. Changing this forces a new resource to be created."
+  type        = string
+  sensitive   = false
+}
+variable "resource_group_name" {
+  description = "The name of the resource group in which the Container App Environment is to be created. Changing this forces a new resource to be created."
+  type        = string
+  sensitive   = false
+}
+variable "log_analytics_workspace_id" {
+  description = "The ID for the Log Analytics Workspace to link this Container Apps Managed Environment to. Changing this forces a new resource to be created."
+  type        = string
+  sensitive   = false
+}
 variable "container_app_environment_storage_name" {
   description = "The name for this Container App Environment Storage. Changing this forces a new resource to be created."
   type        = string
